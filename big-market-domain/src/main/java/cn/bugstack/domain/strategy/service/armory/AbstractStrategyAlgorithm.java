0a04d60d584317252462a753e3626d7cf84493c4
小傅哥 <184172133@qq.com>
2024-11-07 16:12:12 +0800

    Update AbstractStrategyAlgorithm.java

--- big-market-domain/src/main/java/cn/bugstack/domain/strategy/service/armory/AbstractStrategyAlgorithm.java
+++ big-market-domain/src/main/java/cn/bugstack/domain/strategy/service/armory/AbstractStrategyAlgorithm.java
@@ -145,4 +145,27 @@ public abstract class AbstractStrategyAlgorithm implements IStrategyArmory, IStr
         return Math.pow(10, beginLength + endLength);
     }
 
+    // 概率范围值，百分位、千分位、万分位
+    protected double convert(BigDecimal min) {
+        if (BigDecimal.ZERO.compareTo(min) == 0) return 1D;
+
+        String minStr = min.toString();
+
+        // 小数点前
+        String beginVale = minStr.substring(0, minStr.indexOf("."));
+        int beginLength = 0;
+        if (Double.parseDouble(beginVale) > 0) {
+            beginLength = minStr.substring(0, minStr.indexOf(".")).length();
+        }
+
+        // 小数点后
+        String endValue = minStr.substring(minStr.indexOf(".") + 1);
+        int endLength = 0;
+        if (Double.parseDouble(endValue) > 0) {
+            endLength = minStr.substring(minStr.indexOf(".") + 1).length();
+        }
+
+        return Math.pow(10, beginLength + endLength);
+    }
+
 }
